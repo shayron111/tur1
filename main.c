0f29bee64782e2b32dd2572c2093d792b57b8c7c
shayron111 <shsyron232069@gmail.com>
2025-02-01 10:16:35 +0200

    third commit

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+
+
+void print2 ();
 int main (void)
 {
 	printf("shay");
 	printf("hili");
+    print2 ();
 }
---
+++ main2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print2 ()
+{
+	printf ("hili from another file \n" );
+}
