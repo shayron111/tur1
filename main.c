d8b43d2ec8e17b03d3dab814a277a6dde94365f3
shayron111 <shsyron232069@gmail.com>
2025-02-01 18:16:13 +0200

    branch y commit

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 
 void print2 ();
 void print3 ();
+void print4 ();
+
 int main (void)
 {
 	printf("shay");
 	printf("hili");
     print2 ();
     print3 ();
+	print4();
 }
---
+++ main4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+void print4 ();
+void print4 ()
+{
+	printf ("hili from main4 file \n" );
+}
